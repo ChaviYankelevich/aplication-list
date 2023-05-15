29ba9d8f635e8ae5665f9735b89c9ebce3e8472b
chaviyankelevick <chavi95445@gmail.com>
2023-05-15 13:57:34 +0300

    update

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-import express from "express";
-import sdk from 'api';
-
+import express from 'express'
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 const app=express()
-const port=3000
-// const API_KEY="rnd_U7LZ2tFjcogsWlX9PHVZjo0ZKsoN"
-const api = sdk('@render-api/v1.0#dnrc1ulf088q9j');
+const port=3000;
 
-api.auth('rnd_U7LZ2tFjcogsWlX9PHVZjo0ZKsoN');
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
-app.get('/',(rec,res)=>{
-    api.getServices({limit: '20'})
-  .then(({ data }) => res.send(data))
+
+sdk.auth('rnd_AfB7Rcwi3jNhb2Hzb5x2fjWN5bwN');
+sdk.getServices({limit: '20'})
+  .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
-    // res.send("hello world")
+
+  app.get('/',(req,res)=>{
+    res.send('hello world')
 })
 app.listen(port,()=>{
-console.log(`example ${port}`)
+    console.log(`app listening on http://localhost:${port}`)
 })
-
-
-
